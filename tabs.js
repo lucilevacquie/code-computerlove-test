abb5f295c9d915914e513f720350b1db9cabc056
lucilevacquie <lucile.vacquie@reachstudios.co.uk>
2022-09-12 09:27:24 +0100

    add the active class to the first tab and content + start js

--- tabs.js
+++ tabs.js
@@ -1,1 +1,6 @@
-console.log('log from tabs.js');
+function onTabClick(event) {
+    //class 'active' on the selected tab/content (first tab by default)
+    let activeTab = document.querySelector('.active');
+
+
+}
