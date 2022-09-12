afe6b0a82b5dd393c64bfdb49dcea8b0f10fc372
lucilevacquie <lucile.vacquie@reachstudios.co.uk>
2022-09-12 09:31:23 +0100

    createthe function that removes the active class from the tab

--- tabs.js
+++ tabs.js
@@ -2,5 +2,9 @@ function onTabClick(event) {
     //class 'active' on the selected tab/content (first tab by default)
     let activeTab = document.querySelector('.active');
 
-
+    //first, remove the active class that already exists
+    //loop through all the tabs and replace 'active' by empty string
+    activeTab.forEach(function (tab) {
+        tab.className = tab.className.replace('active', '');
+    })
 }
