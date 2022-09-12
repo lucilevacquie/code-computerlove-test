f57d3df06008d32f1be339033cb409ac0a4bbf51
lucilevacquie <lucile.vacquie@reachstudios.co.uk>
2022-09-12 09:36:37 +0100

    create the logic to add active to class

--- tabs.js
+++ tabs.js
@@ -7,4 +7,9 @@ function onTabClick(event) {
     activeTab.forEach(function (tab) {
         tab.className = tab.className.replace('active', '');
     })
+
+    //then, add the 'active' class to the selected tab
+    event.target.parentElement.className += ' active';
+    //and add the 'active' class to the coresponding content element
+    document.getElementById(event.target.id.split('button-')[1]).className += ' active';
 }
