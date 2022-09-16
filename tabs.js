3159814181ff618b196b1b007ff3ef4677809c10
lucilevacquie <lucile.vacquie@reachstudios.co.uk>
2022-09-16 09:30:20 +0100

    create the accordion for mobile screen

--- tabs.js
+++ tabs.js
@@ -1,50 +1,70 @@
 const data = {
     tab1: {
         title: "Tab 1",
-        content: "Hello"
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     tab2: {
         title: "Tab 2",
-        content: "Hola"
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     tab3: {
         title: "Tab 3",
-        content: "Bonjour"
-    },
-    tab4: {
-        title: "Tab 4",
-        content: "Guten Tag"
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const tabWrapper = document.getElementById("tab-wrapper");
     const content = document.getElementById("content");
+    const mobileContent = document.getElementById("mobile-content");
 
     // set the default content to the first tab content
     content.innerHTML = data.tab1.content;
 
     Object.keys(data).forEach(id => {
         const tabItem = data[id];
-        //create the tab element and set its attributes
+
+        // create the tab element and set its attributes
         const newTab = document.createElement('li');
         newTab.setAttribute('id', id);
         newTab.setAttribute('class', 'tab');
-        newTab.setAttribute('aria-selected', false);
-        newTab.setAttribute('aria-label', tabItem.title);
-        newTab.innerText = tabItem.title;
 
+        // create the tab title element and set it's attributes
+        const newTabTitle = document.createElement('div')
+        newTabTitle.setAttribute('class', 'tab-title');
+        newTabTitle.setAttribute('aria-selected', false);
+        newTabTitle.setAttribute('aria-label', tabItem.title);
+        newTabTitle.innerText = tabItem.title;
+
+        // on click handler 
         newTab.addEventListener('click', () => {
+            // create an array of tabs
+            // loop through it 
+            // remove the active class
+            // set aria-selected to false
             Array.from(document.getElementsByClassName('tab')).forEach(element => {
                 element.className = element.className.replace('active', '');
                 element.setAttribute('aria-selected', false);
-            })
+            });
+
+            // active tab -- set active class and accessibility
             newTab.className += ' active';
             newTab.setAttribute('aria-selected', true);
+
+            // append the mobile content after the active tab
+            newTab.after(mobileContent);
+            // display tab content
+            mobileContent.className += ' show';
+
+            // set tab content for mobile
+            mobileContent.innerHTML = tabItem.content;
+
+            // set tab content for tablet/desktop
             content.innerHTML = tabItem.content;
         })
 
         tabWrapper.appendChild(newTab);
+        newTab.appendChild(newTabTitle);
     });
 
     // default first tab to active
